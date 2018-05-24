77f0c3f09e96d187ca7a19454c71c84cb88669fc
NeYziX <38168101+NeYziX@users.noreply.github.com>
2018-05-24 20:21:01 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -35,12 +35,12 @@ function sendError(message, description) {
 
 bot.on('message', message => {
     if(message.content[0] === prefix) {
-      let splitMessage = message.content.split(" ");
-      if(splitMessage[0] === "</ban") {
-          if(splitMessage.length === 2)
-              message.guild.ban(message.mentions.users.firts());
-          else
-              sendError(message, "Erreur");                    
-      }
+        let splitMessage = message.content.split(" ");
+        if(splitMessage[0] === "</ban") {
+            if(splitMessage.length === 2)
+               message.guild.ban(message.mentions.users.first());
+            else
+                sendError(message, "Erreur");                    
+        }
     }
 })
