de382f44d4e308c19e8975c8b3284ea04a497c86
NeYziX <38168101+NeYziX@users.noreply.github.com>
2018-05-24 20:01:48 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -15,6 +15,7 @@ bot.on('message', message => {
 
     if (message.content === prefix + "help") {
         var embed = new Discord.RichEmbed()
+        .setAuthor("Nom", "https://cdn.discordapp.com/attachments/442741695289753611/449270732665651211/natsu.png")
         .setTitle("Commande du UltraROBOT !")
         .addField("</help", "Affiche la liste des commandes du bot !")
         .setFooter("© </UltraDév'>")
@@ -22,43 +23,5 @@ bot.on('message', message => {
         .setTimestamp()
         .setThumbnail(message.guild.iconURL)
     message.channel.sendEmbed(embed)
-    }
-});
-
-bot.on("message", function(message) {
-    if (message.author.equals(bot.user)) return;
-  
-    if (!message.content.startsWith(prefix)) return;
-  
-    var args = message.content.substring(prefix.length).split(" ");
-
-    switch (args[0].toLowerCase()) {
-        case "8ball":
-            let args = message.content.split(" ").slice(1);
-            let tte = args.join(" ")
-            if (!tte){
-        
-        
-            var replys = [
-                "Oui",
-                "Non",
-                "Je ne sais pas",
-                "Surement"
-            ];
-        
-            let reponse = (replys[Math.floor(Math.random() * replys.length)])
-            var bembed = new Discord.RichEmbed()
-            .setDescription(":8ball: 8ball")
-            .addField("Question :thinking:", tte)
-            .addField("Réponse :kissing_heart:", reponse)
-            message.channel.sendEmbed(bembed)
-            break;
-            case "clear":
-            if (message.member.hasPermission("MANAGE_MESSAGES")){
-                message.channel.fetchMessages()
-                    .then(function(list){
-                        message.channel.bulkDelete(list);
-                }, function(err){message.channel.send("Erreur")})}
-            break;
     }
 });
