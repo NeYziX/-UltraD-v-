798fce5e6ea89c67511d2ee589536e7cbf847b74
NeYziX <38168101+NeYziX@users.noreply.github.com>
2018-05-24 19:40:40 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,12 +1,26 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-var prefix = ("<!");
+var prefix = ("/");
 
 bot.on("ready", () => {
   
   console.log("Salut"); 
-  bot.user.setGame("<!help | Dév' By NeYziX ✨")
+  bot.user.setGame("/help | Dév' By NeYziX ✨")
 });
 
 bot.login(process.env.BOT_TOKEN);
+
+bot.on('message', message => {
+
+    if (message.content === prefix + "help") {
+        var embed = new Discord.RichEmbed()
+        .setTitle("Commande du ZBot !")
+        .addField("/help", "Affiche la liste des commandes du bot !")
+        .setFooter("© ZDown")
+        .setColor("0xd26cba")
+        .setTimestamp()
+        .setThumbnail(message.guild.iconURL)
+    message.channel.sendEmbed(embed)
+    }
+});
