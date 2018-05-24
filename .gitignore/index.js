e2a8208e1e147cfefbf65773815248f40b67363b
NeYziX <38168101+NeYziX@users.noreply.github.com>
2018-05-24 19:58:51 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -24,3 +24,41 @@ bot.on('message', message => {
     message.channel.sendEmbed(embed)
     }
 });
+
+bot.on("message", function(message) {
+    if (message.author.equals(bot.user)) return;
+  
+    if (!message.content.startsWith(prefix)) return;
+  
+    var args = message.content.substring(prefix.length).split(" ");
+
+    switch (args[0].toLowerCase()) {
+        case "8ball":
+            let args = message.content.split(" ").slice(1);
+            let tte = args.join(" ")
+            if (!tte){
+        
+        
+            var replys = [
+                "Oui",
+                "Non",
+                "Je ne sais pas",
+                "Surement"
+            ];
+        
+            let reponse = (replys[Math.floor(Math.random() * replys.length)])
+            var bembed = new Discord.RichEmbed()
+            .setDescription(":8ball: 8ball")
+            .addField("Question :thinking:", tte)
+            .addField("Réponse :kissing_heart:", reponse)
+            message.channel.sendEmbed(bembed)
+            break;
+            case "clear":
+            if (message.member.hasPermission("MANAGE_MESSAGES")){
+                message.channel.fetchMessages()
+                    .then(function(list){
+                        message.channel.bulkDelete(list);
+                }, function(err){message.channel.send("Erreur")})}
+            break;
+    }
+});
