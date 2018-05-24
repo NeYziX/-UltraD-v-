7f951d3d2544e6699778860dcbd412e1947c2720
NeYziX <38168101+NeYziX@users.noreply.github.com>
2018-05-24 19:52:08 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,12 +1,12 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-var prefix = ("/");
+var prefix = ("</");
 
 bot.on("ready", () => {
   
   console.log("Salut"); 
-  bot.user.setGame("/help | Dév' By NeYziX ✨")
+  bot.user.setGame("</help | Dév' By NeYziX ✨")
 });
 
 bot.login(process.env.BOT_TOKEN);
@@ -15,10 +15,10 @@ bot.on('message', message => {
 
     if (message.content === prefix + "help") {
         var embed = new Discord.RichEmbed()
-        .setTitle("Commande du ZBot !")
-        .addField("/help", "Affiche la liste des commandes du bot !")
-        .setFooter("© ZDown")
-        .setColor("0xd26cba")
+        .setTitle("Commande du UltraROBOT !")
+        .addField("</help", "Affiche la liste des commandes du bot !")
+        .setFooter("© </UltraDév'>")
+        .setColor("0x6a9ccc")
         .setTimestamp()
         .setThumbnail(message.guild.iconURL)
     message.channel.sendEmbed(embed)
